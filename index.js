547055c6808aeb9067f7e0e2a4123a531d7e3b2f
Věra Juračáková <juracakova.vera@gmail.com>
2024-12-08 20:35:53 +0100

    Postup řešení cvičení 08.12.2024

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('funguju!');
+const budik = document.querySelector(".alarm")
+const pocetSekund = prompt("Zadejte počet sekund:")
+const stop = document.querySelector("#stop")
+
+stop.addEventListener("click", () => {
+    clearTimeout(odpocet)
+})
+
+const odpocet = setTimeout(() => {
+    budik.classList.add("alarm--ring")
+}, pocetSekund * 1000)
+
+//clearTimeout - zrušení časovače
